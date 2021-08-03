be6ec9e5e88397f725bff6281e31d70e4ddb604b
유수현 <rhan0871@naver.com>
2021-08-03 14:20:07 +0900

    -

--- test.py
+++ test.py
@@ -1,9 +1,1 @@
-t1, t2=None, 10
-
-def test(t1,t2):
-    if t1 and t2:
-        return t1+t2
-    else:
-        return t1 or t2
-
-print(test(t1,t2))
+print('Absurd')
--- 트리/트리_LeetCode_297.py
+++ 트리/트리_LeetCode_297.py
@@ -1,9 +1,10 @@
-# Definition for a binary tree node.
+from collections import deque
+
 class TreeNode(object):
-    def __init__(self, x):
-        self.val = x
-        self.left = None
-        self.right = None
+    def __init__(self,x) -> None:
+        self.val=x
+        self.left=None
+        self.right=None
 
 class Codec:
 
@@ -13,7 +14,22 @@ class Codec:
         :type root: TreeNode
         :rtype: str
         """
+        if not root:
+            return '#'
+        deq=deque([root])
+        result=['#']
+
+        while deq:
+            node=deq.popleft()
+            if node:
+                result.append(str(node.val))
+                deq.append(node.left)
+                deq.append(node.right)
+            else:
+                result.append('#')
         
+        return result
+
 
     def deserialize(self, data):
         """Decodes your encoded data to tree.
@@ -21,9 +37,26 @@ class Codec:
         :type data: str
         :rtype: TreeNode
         """
-        
+        if data=='#':
+            return None
+        deq=deque(data[1:])
+        root=TreeNode(deq.popleft())
 
-# Your Codec object will be instantiated and called as such:
-# ser = Codec()
-# deser = Codec()
-# ans = deser.deserialize(ser.serialize(root))
+        index,trees=0,[root]
+        
+        while deq:
+            node=deq.popleft()
+            node2=deq.popleft()
+
+            if node!='#':
+                tree=TreeNode(node)
+                trees[index].left=tree
+                trees.append(tree)
+            if node2!='#':
+                tree2=TreeNode(node2)
+                trees[index].right=tree2
+                trees.append(tree2)
+            
+            index+=1
+
+        return trees[0]
