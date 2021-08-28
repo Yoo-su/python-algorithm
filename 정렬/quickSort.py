60ba711a1b6f07962ae17414fffa7b1ff31b71cd
Yoo-su <57261470+Yoo-su@users.noreply.github.com>
2021-08-28 22:35:05 +0900

    Update quickSort.py

--- 정렬/quickSort.py
+++ 정렬/quickSort.py
@@ -1,32 +1,23 @@
-def quickSort(nums):
-    def partition(l, r):
-        left,right=l,r
-        if left>=right:
-            return
+def quick(nums, left, right):
+    if left>=right:
+        return
+    
+    pivot=nums[left]
+    print(pivot)
+    l, r = left+1, right
+
+    while l<r:
+        while l<right and nums[l]<=pivot:
+            l+=1
+        while r>left and nums[r]>pivot:
+            r-=1
+
+        if l<r:
+            nums[l] , nums[r] = nums[r] , nums[l]
+
+    if pivot>nums[r]:
+        nums[left], nums[r] = nums[r], pivot
         
-        pivot=nums[l]
-        
-        while left<right:
-            while nums[left]<=pivot and left<r:
-                left+=1
-            while nums[right]>=pivot and right>l:
-                right-=1
-
-            #left, right 값 swap
-            if left<=right:
-                nums[left], nums[right]= nums[right], nums[left]
-
-        #pivot 배치
-        nums[right],nums[l]=nums[l], nums[right]
-        
-        partition(l,right-1)
-        partition(right+1,r)
-
-    partition(0,len(nums)-1)
-
-    return nums
-
-test=[5,4,3,2,1,8,8,9,11,11,7,6]
-
-print(quickSort(test))
+    quick(nums,left,r-1)
+    quick(nums,r+1,right)
 
