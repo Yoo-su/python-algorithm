277a93b11eae7f6ca7ef1a20506e7254eb3cfd12
유수현 <rhan0871@naver.com>
2021-09-03 12:29:04 +0900

    -

---
+++ 이진 검색/이진 검색_LeetCode_33.py
@@ -0,0 +1,34 @@
+from typing import List
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        left, right = 0, len(nums)-1
+        while left<right:
+            mid=left+(right-left)//2
+
+            #중간값이 맨 우측보다 크다는 건 그 사이에 피봇이 있다는 것.
+            if nums[mid]>nums[right]:
+                left=mid+1
+            #반대의 경우
+            else:
+                right=mid
+
+        pivot=left
+
+        left, right = 0, len(nums)-1
+        while left<=right:
+            mid=left+(right-left)//2
+
+            mid_pivot=(mid+pivot)%len(nums)
+
+            if nums[mid_pivot]<target:
+                left=mid+1
+            elif nums[mid_pivot]>target:
+                right=mid-1
+            else:
+                return mid_pivot
+        return -1
+
+
+        
+
